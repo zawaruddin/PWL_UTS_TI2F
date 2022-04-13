05689766f0965182dadd6ffe288650a84b8fa8df
tzelginianc <tzelginia78@gmail.com>
2022-04-13 13:53:33 +0700

    route 18

--- routes/18.php
+++ routes/18.php
@@ -4,16 +4,17 @@
 
     Catatan, lengkapi data diri anda berikut ini
     
-    NIM     : 
-    Nama    : 
+    NIM     : 2041720048
+    Nama    : Tzelginia Noer Cahyani
     Kelas   : TI 2F
-    Nama sesuai folder controller : 
+    Nama sesuai folder controller : tzelginia_18
     
 */
 
 //use App\Http\Controllers\nama_no\NamaController;
-
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\tzelginia_18\SupermarketController_18;
 // silahkan tulis code router dibawah ini 
+
+Route::resource('tzelginia_18', SupermarketController_18::class);
